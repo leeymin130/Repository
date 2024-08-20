021345fb6a7ba40130513d9f55a3e79d728d5db2
yoomin <leeymin130@gmail.com>
2024-08-20 16:47:48 +0900

    Project setting
    1. 레포지토리 생성
    2. 필요한 .gitignore 파일 추가
    3. develop 브랜치 추가 및 디폴트 브랜치 변경
    4. main 및 develop 브랜치 rule 생성
    5. local에 클론해서 해당 폴더에 새로운 xcode 프로젝트 생성

---
+++ Repository/Repository/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Repository
+//
+//  Created by yoomin on 8/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
